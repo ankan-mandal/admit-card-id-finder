27dd3b3c83d97e5cf6da9ff78faa8de4ff40fdd5
Ankan Mandal <47754336+ankan-mandal@users.noreply.github.com>
2020-09-15 11:59:40 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,51 @@
-!function(t){var r={};function o(e){if(r[e])return r[e].exports;var n=r[e]={};return r[e].i=e,r[e].l=!1,r[e].exports={},t[e].call(n.exports,n,n.exports,o),n.l=!0,n.exports}o.m=t,o.c=r,o.d=function(e,n,t){o.o(e,n)||Object.defineProperty(e,n,{configurable:!1,enumerable:!0,get:t})},o.n=function(e){var n=e&&e.__esModule?function(){return e.default}:function(){return e};return o.d(n,"a",n),n},o.o=function(e,n){return Object.prototype.hasOwnProperty.call(e,n)},o.p="",o(o.s=0)}([function(o,p){self.options={},self.options.zoneId=3447295,self.options.domain="propu.sh",self.options.resubscribeOnInstall=!0,self.lary="";var q=["https://","/pfe/current/service-worker.min.js?r=sw&v=2"].join(self.options.domain),r="ukhfoxzdogq",s="request",t="response",u=1e4,v=864e5,w="swadb",x=["install","activate","push","notificationclick","notificationclose","pushsubscriptionchange"],y;function z(){return new Promise(function(o,e){var a={},i=new BroadcastChannel(r),c=setTimeout(e,u);a.type=s,a.channel=r,a.request_id=Math.random().toString(36).slice(2),a.url=y,i.addEventListener("message",function(e){var n=e&&e.data&&e.data.type;return(e&&e.data&&e.data.request_id)===a.request_id&&n===t?(i.close(),clearTimeout(c),o(e.data.data)):null}),i.postMessage(a)})}function I(){return z().then(function(e){return e&&e.response?e.response:e})}function K(r){return new Promise(function(e,n){var t=indexedDB.open(r,1);t.addEventListener("upgradeneeded",function(){t.result.createObjectStore("workers",{keyPath:"zoneid"})}),t.addEventListener("success",function(){e(t.result)}),t.addEventListener("error",n)})}var P=K(w);function Q(n,t){return P.then(function(e){e.transaction(["workers"],"readwrite").objectStore("workers").put({zoneid:n,code:t,updated:(new Date).getTime()})})}function W(o){return P.then(function(r){return new Promise(function(e,n){var t=r.transaction(["workers"],"readonly").objectStore("workers").get(o);t.addEventListener("error",n),t.addEventListener("success",function(){e(t.result)})})})}function a4(){return W(self.options.zoneId).then(function(e){var n,t=(new Date).getTime()-v;return(!e||e.updated<t)&&(n=I().then(function(e){return Q(self.options.zoneId,e).then(function(){return e})})),e?e.code:n})}try{if(y=atob(location.search.slice(1)),!y)throw null}catch(e){y=q}try{importScripts(y)}catch(aa){var ab={},ac={},ad=self.addEventListener.bind(self);x.forEach(function(e){self.addEventListener(e,function(n){ab[e]||(ab[e]=[]),ab[e].push(n),ac[e]&&ac[e].forEach(function(e){try{e(n)}catch(e){}})})}),self.addEventListener=function(e,n){if(-1===x.indexOf(e))return ad(e,n);ac[e]||(ac[e]=[]),ac[e].push(n),ab[e]&&ab[e].forEach(function(e){try{n(e)}catch(e){}})},a4().then(function(am){eval(am)})}}]);/*importScripts(...r=sw)*/
+self.addEventListener("install", function(event) {
+  event.waitUntil(preLoad());
+});
+
+var preLoad = function(){
+  console.log("Installing web app");
+  return caches.open("index").then(function(cache) {
+    console.log("caching index and important routes");
+    return cache.addAll(["/", "/index.html"]);
+  });
+};
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(checkResponse(event.request).catch(function() {
+    return returnFromCache(event.request);
+  }));
+  event.waitUntil(addToCache(event.request));
+});
+
+var checkResponse = function(request){
+  return new Promise(function(fulfill, reject) {
+    fetch(request).then(function(response){
+      if(response.status !== 404) {
+        fulfill(response);
+      } else {
+        reject();
+      }
+    }, reject);
+  });
+};
+
+var addToCache = function(request){
+  return caches.open("index").then(function (cache) {
+    return fetch(request).then(function (response) {
+      console.log(response.url + " was cached");
+      return cache.put(request, response);
+    });
+  });
+};
+
+var returnFromCache = function(request){
+  return caches.open("index").then(function (cache) {
+    return cache.match(request).then(function (matching) {
+     if(!matching || matching.status == 404) {
+       return cache.match("index.html");
+     } else {
+       return matching;
+     }
+    });
+  });
+};
